c96668e358d5d209b8f04539afb056435b269b97
Myprofilee <andrep13.id@gmail.com>
2023-04-11 17:59:42 +0700

    first commit

--- assets/js/home/myzuu-event.js
+++ assets/js/home/myzuu-event.js
@@ -1,9 +1,17 @@
 $(function () {
     var bar = '';
 
+    bar += '<a href="https://myzuu.webtoko.net/digital/201174">';
+        bar += '<button class="button-myzuu-produk2">';
+            bar += '<img src="https://sc.myzuu.my.id/assets/img/home/slider/pubg.png" alt="Myzuu">';
+            bar += '<p>Kini Myzuu menyediakan TopUp Game Pubg Mobile Region Indonesia</p>';
+            bar += '<h4>Yuk buruan topup sekarang!</h4>';
+        bar += '</button>';
+    bar += '</a>';
+
     bar += '<a href="https://myzuu.webtoko.net/digital/200540">';
         bar += '<button class="button-myzuu-produk2">';
-            bar += '<img src="https://sc.myzuu.my.id/assets/img/home/event/1.png" alt="Myzuu">';
+            bar += '<img src="https://sc.myzuu.my.id/assets/img/home/slider/allstar-themed.png" alt="Myzuu">';
             bar += '<p>Skin Miya "Pop Atomico Miya" & Eudora "Pop Atomico Eudora</p>';
             bar += '<h4>Klik, Punya Kode Voucher? Gunakan skrng</h4>';
         bar += '</button>';
